d52f0858f5ec4f14320876e3ef7a1b608ab9938c
Brett Pelkey <btp974@gmail.com>
2022-02-21 18:13:51 -0700

    updated keymap.c

--- keyboards/ferris/keymaps/broskie/keymap.c
+++ keyboards/ferris/keymaps/broskie/keymap.c
@@ -51,15 +51,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_GESC, KC_HOME, KC_END, KC_PGUP, KC_PGDN,          KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_QUOT,
+    KC_GESC, KC_HOME, LSFT_T(KC_END), KC_PGUP, KC_PGDN,  KC_MINS, KC_EQL,  LSFT_T(KC_LPRN), KC_RPRN, KC_QUOT,
     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_ENT,
                                     KC_SWAP, _______, KC_DELETE, _______  
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_F11,  KC_F12,  _______, _______, _______,         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-    _______, _______, _______, _______, _______,         _______, _______, _______, KC_PIPE, KC_ENT,
+    KC_TAB,  KC_F11,  LSFT_T(KC_F12), _______, _______,  KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    _______, _______, _______, _______, _______,         KC_PIPE, _______, _______, _______, KC_ENT,
                                     _______, TT(_SPECL), _______, _______
   ),
 
