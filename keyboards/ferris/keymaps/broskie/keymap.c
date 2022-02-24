a54919fa753b9216ebee115bdf6f12758a1fb154
Brett Pelkey <btp974@gmail.com>
2022-02-23 20:55:00 -0700

    added num layer

--- keyboards/ferris/keymaps/broskie/keymap.c
+++ keyboards/ferris/keymaps/broskie/keymap.c
@@ -12,7 +12,8 @@ enum ferris_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _SPECL
+  _SPECL,
+  _NUM
 };
 
 enum ferris_tap_dances {
@@ -51,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_GESC, KC_HOME, LSFT_T(KC_END), KC_PGUP, KC_PGDN,  KC_MINS, KC_EQL,  LSFT_T(KC_LPRN), KC_RPRN, KC_QUOT,
+    KC_GESC, KC_HOME, LSFT_T(KC_END), KC_PGUP, KC_PGDN,  KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_QUOT,
     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_ENT,
                                     KC_SWAP, _______, KC_DELETE, _______  
   ),
@@ -60,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     KC_TAB,  KC_F11,  LSFT_T(KC_F12), _______, _______,  KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     _______, _______, _______, _______, _______,         KC_PIPE, _______, _______, _______, KC_ENT,
-                                    _______, TT(_SPECL), _______, _______
+                                    _______, TT(_SPECL), _______, TT(_NUM)
   ),
 
   [_SPECL] = LAYOUT( /* [> SPECL <] */
@@ -68,6 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MS_BTN1,         _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,         _______, _______, _______, _______, RESET,
                                     _______, _______, _______, _______
+  ),
+
+  [_NUM] = LAYOUT( /* [> NUM <] */
+    TG(_NUM), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_7, KC_8, KC_9, KC_MINS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_4, KC_5, KC_6, KC_DOT,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_1, KC_2, KC_3, KC_0,
+                                    XXXXXXX, KC_SPL1, KC_BSL2, XXXXXXX
   )
 };
 
