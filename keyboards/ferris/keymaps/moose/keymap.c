01f2cfb64f7328f7a41550a1aea406d087ea935e
jharrod <jharrod@users.noreply.github.com>
2022-02-27 00:08:24 -0700

    upate moose

--- keyboards/ferris/keymaps/moose/keymap.c
+++ keyboards/ferris/keymaps/moose/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,       KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,   KC_8,    KC_9,    KC_0,
     _______, _______, _______, _______, _______,         _______, KC_ENT, KC_MINS, KC_EQL, KC_BSLS,
     KC_GRV,  _______, _______, _______, _______,         _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,
-                               _______, KC_LSFT,         RES, _______
+                               _______, KC_LSFT,         _______, RES
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
