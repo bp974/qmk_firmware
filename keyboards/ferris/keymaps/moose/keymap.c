9f9f45859fb76aef66afdf3db06e620a543209c5
jharrod <jharrod@users.noreply.github.com>
2022-03-21 12:08:42 -0600

    update moose keymaps

--- keyboards/ferris/keymaps/moose/keymap.c
+++ keyboards/ferris/keymaps/moose/keymap.c
@@ -94,21 +94,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , _______, _______, _______, _______,        _______, _______, _______, KC_MINS, KC_EQL,
     KC_CAPS, _______, _______, _______, _______,        _______, _______, _______, _______, KC_QUOT,
     KC_GRV,  _______, _______, _______, _______,        _______, _______, KC_LBRC, KC_RBRC, KC_BSLS,
-                             KC_DELETE, KC_LSFT,         RES, _______
+                             KC_DELETE, XXXXXXX,        RES, _______
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_ESC,   _______, _______, _______, _______,        _______, _______, _______, _______, _______,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     _______, _______, COPY ,   PASTE, _______,           _______, _______, _______, KC_F11,  KC_F12,
-                              _______, KC_LSFT,          _______,_______
+                              KC_LSFT, XXXXXXX,          _______,_______
   ),
 
   [_SPACE] = LAYOUT( /* [> SPACE <] */
     KC_1,       KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,   KC_8,    KC_9,    KC_0,
     KC_TAB,  _______, _______, _______, KC_PGUP,         KC_HOME, KC_LEFT,  KC_UP , KC_DOWN, KC_RIGHT,
     KC_CAPS, _______, _______, _______, KC_PGDN,         KC_END , _______, _______, _______, _______,
-                               _______, KC_LSFT,         _______, _______
+                               KC_LSFT, XXXXXXX,         _______, _______
   ),
 
   [_RESET] = LAYOUT(
