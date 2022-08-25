d094d2b099326bf9f992459a1537c514c1da777c
jharrod <jharrod@users.noreply.github.com>
2022-08-24 21:03:36 -0600

    update moose keymaps

--- keyboards/handwired/dactyl_manuform/5x6_6/keymaps/moose/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_6/keymaps/moose/keymap.c
@@ -23,23 +23,23 @@
 #define T_DEL MT(MOD_LALT,KC_DEL)
 
 // Layer switches
-#define EMODT LT(_MOOSE, KC_E)
+// #define EMODT LT(_MOOSE, KC_E)
 #define CAPST LT(_MOOSE, KC_CAPS)
-#define RMODT LT(_RAISE, KC_R)
-#define SPCET LT(_SPACE, KC_SPC)
+// #define RMODT LT(_RAISE, KC_R)
+#define SPCET LT(_MOOSE, KC_SPC)
 
 #define MEHT MEH_T(KC_T)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_EQL , KC_1  , KC_2  , KC_3  , KC_4 , KC_5  ,                         KC_6  ,  KC_7  ,  KC_8  ,  KC_9  , KC_0  ,  KC_MINS,
-        KC_TAB , KC_Q , KC_W  , EMODT , RMODT , MEHT  ,                         KC_Y  ,  KC_U  ,  KC_I  ,  KC_O  , KC_P  ,  KC_BSLS,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R , MEHT  ,                         KC_Y  ,  KC_U  ,  KC_I  ,  KC_O  , KC_P  ,  KC_BSLS,
         CAPST  , HRL1  , HRL2  , HRL3  , HRL4 , KC_G  ,                         KC_H  ,  HRR1  ,  HRR2  ,  HRR3  , HRR4  ,  KC_QUOT,
         KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V , KC_B  ,                         KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                 KC_GRV, KC_INS,KC_LEFT,KC_RGHT,                                          KC_UP, KC_DOWN ,KC_LBRC, KC_RBRC,
+                 KC_GRV, TG(LOWER),KC_LEFT,KC_RGHT,                                          KC_UP, KC_DOWN ,KC_LBRC, KC_RBRC,
 
-                                       KC_ESC, KC_LALT,                        MOD_RGUI, KC_LCTL,
-                                                _______,                        TT(LOWER),
+                                       KC_ESC, MEH(KC_Q),                      MEH(KC_E), MEH(KC_0),
+                                               MEH(KC_W),                      MEH(KC_R),
                                   TBSPC, T_DEL, KC_HOME,                       KC_END, KC_ENTER, SPCET
     ),
 
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,_______,                        _______, KC_P7 ,  KC_P8 , KC_P9 ,KC_PMNS,_______,
         _______,_______,_______,_______,_______,_______,                        _______, KC_P4 ,  KC_P5 , KC_P6 ,KC_PPLS,_______,
         _______,_______,_______,_______,_______,_______,                        _______, KC_P1 ,  KC_P2 , KC_P3 ,KC_PENT,_______,
-                _______,_______,_______,_______,                                         _______,_______,KC_PDOT,KC_PENT,
+                _______,_______,_______,_______,                                         _______, KC_P0 , KC_PDOT,KC_PENT,
 
                                         _______,_______,                        _______,TT(_QWERTY),
                                                 _______,                        _______,
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MOOSE] = LAYOUT_5x6(
           KC_ESC , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-          KC_F12 ,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,                        _______,_______,KC_PGUP,_______,_______,_______,
-          _______,_______,KC_MPLY,KC_MPRV,KC_MNXT,_______,                        KC_HOME, KC_LEFT,KC_UP ,KC_DOWN,KC_RIGHT,KC_END,
-          _______,_______,_______,_______,_______,_______,                        _______,_______,KC_PGDN,_______,_______,_______,
-                  _______,_______,_______,_______,                                        _______,_______,_______,_______,
+          KC_F12 ,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,                        _______,_______,KC_UP,_______,_______,_______,
+          _______,_______,KC_MPLY,KC_MPRV,KC_MNXT,_______,                        KC_HOME,KC_LEFT,KC_DOWN,KC_RIGHT,_______,KC_END,
+          _______,_______,_______,_______,_______,_______,                        _______,KC_PGUP,_______,_______,_______,_______,
+                  _______,_______,_______,_______,                                        KC_PGDN,_______,_______,_______,
 
                                           _______,_______,                        _______,_______,
                                                   _______,                        _______,
